feaa0b4759b66e78b0b7119e4c4294120a9dec8f
kamiljanek <gttony@o2.pl>
2022-02-20 21:55:43 +0100

    initial commit

---
+++ MatchingGame-WORDS/MatchingGame-WORDS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MatchingGame_WORDS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
